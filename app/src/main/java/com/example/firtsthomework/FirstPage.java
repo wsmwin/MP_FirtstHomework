ff52333a8cc0e5b8a9f939d2a19b478b5e3b1583
wsmwin <wsnwin@naver.com>
2019-10-12 16:21:50 +0900

    회원가입, 로그인 조건에 따라 구현 완료, 메인페이지 남음

--- app/src/main/java/com/example/firtsthomework/FirstPage.java
+++ app/src/main/java/com/example/firtsthomework/FirstPage.java
@@ -1,7 +1,6 @@
 package com.example.firtsthomework;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,8 +9,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.util.TreeMap;
+import java.util.HashMap;
 
 public class FirstPage extends AppCompatActivity {
 
@@ -20,35 +18,47 @@ public class FirstPage extends AppCompatActivity {
     Button b_write, b_read, b_viewDB;
     TextView tv_text;
 
-//    TreeMap<String, String> map = new TreeMap<>();
-//    for(int i = 0; i < ~~~.lenght(); i++) {
-//        map.put((~~~~))
-//    }
-
-    //    JSONObject resultObject = json.getJSONObject(0);
-    //    for (int j = 0; j < resultObject.length(); j++) {
-    //        map.put(resultObject.names().getString(j), resultObject.getString(resultObject.names().getString(j)));
-    //    }
+    String tempMember = "";
+    String [] splitMember;
+    HashMap<String, String> map = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_page);
 
-        et_text = (EditText) findViewById(R.id.etEmail);
-        et_text2 = (EditText) findViewById(R.id.etPassword);
-        b_write = (Button) findViewById(R.id.btnRegist);
-        b_read = (Button) findViewById(R.id.btnLogin);
-        b_viewDB = (Button) findViewById(R.id.btnViewDB);
-        tv_text = (TextView) findViewById(R.id.tv_text);
+        et_text = findViewById(R.id.etEmail);
+        et_text2 = findViewById(R.id.etPassword);
+        b_write = findViewById(R.id.btnRegist);
+        b_read = findViewById(R.id.btnLogin);
+        b_viewDB = findViewById(R.id.btnViewDB);
+        tv_text = findViewById(R.id.tv_text);
 
         // 로그인 버튼
         b_read.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                // 로그인 버튼 누를 시 파일 읽어와서 변수에 저장시키고 있나 일치하는 것 있으면 저장.
-                Intent intent = new Intent(getApplicationContext(), MainPage.class);
-                startActivity(intent);
+                // 파일 읽어온 것에서 변수로 담아서 처리해줄 부분
+                tempMember = readFile(fileName);
+                // 읽은 텍스트 파일에서 map에 key로 id값을, value로 비밀번호를 넣는다.
+                splitMember = tempMember.split("\n");
+                for(int i = 0; i < splitMember.length; i++) {
+                    map.put(splitMember[i].split(" ")[0], splitMember[i].split(" ")[1]);
+                }
+                // 아이디와 비밀번호 일치하는지 확인 후 메인 페이지로 넘어가도록 한다. 아니라면 실패 Toast
+                try {
+                    if ( map.get(et_text.getText().toString()).equals(et_text2.getText().toString()) ) {
+                        Toast.makeText(FirstPage.this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), MainPage.class);
+                        startActivity(intent);
+                    }
+                    else {
+                        Toast.makeText(FirstPage.this,"로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                catch(Exception e) {
+                    Toast.makeText(FirstPage.this,"로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -65,7 +75,9 @@ public class FirstPage extends AppCompatActivity {
         b_viewDB.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                // DB 보기 버튼 클릭시 현재 member.txt 파일 저장된 것 보여줌.
+                Toast.makeText(FirstPage.this, "member.txt 파일을 불러왔습니다.", Toast.LENGTH_SHORT).show();
+                splitMember = tempMember.split("\n");
+//                 DB 보기 버튼 클릭시 현재 member.txt 파일 저장된 것 보여줌.
                 tv_text.setText(readFile(fileName));
             }
         });
@@ -83,7 +95,7 @@ public class FirstPage extends AppCompatActivity {
             text = new String(buffer);
         } catch (Exception e) {
             e.printStackTrace();
-            Toast.makeText(FirstPage.this, "Error reading file!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(FirstPage.this, "MEMBER가 아직 없거나 읽을 수 없습니다.", Toast.LENGTH_SHORT).show();
         }
 
         return text;
--- app/src/main/java/com/example/firtsthomework/SignonPage.java
+++ app/src/main/java/com/example/firtsthomework/SignonPage.java
@@ -2,54 +2,103 @@ package com.example.firtsthomework;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.regex.Pattern;
 
 public class SignonPage extends Activity {
 
-    EditText et_Email, et_pass, et_name, et_add, et_tel;
-    Button b_checkEmail, b_checkPass, b_done, b_back;
-    Boolean bool_checkEmail, bool_checkPass, bool_policy;
-    RadioButton rb_policyDec, rb_policyAcc;
+    String fileName = "member.txt";
 
+    EditText et_Email, et_pass, et_name, et_add, et_tel;
+    TextView tv_validEmail, tv_validPass;
+    Button b_done, b_back;
+    Boolean bool_checkEmail, bool_checkPass, bool_policy;
+    RadioGroup rg_policy;
+
+    String [] splitMember;
+    String [] memberIDs = new String[100];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signon_page);
 
-        bool_checkEmail = Boolean.FALSE;
-        bool_checkPass = Boolean.FALSE;
-        bool_policy = Boolean.FALSE;
+        bool_checkEmail = false;
+        bool_checkPass = false;
+        bool_policy = false;
 
-        et_Email = (EditText) findViewById(R.id.etEmail);
-        et_pass = (EditText) findViewById(R.id.etPassword);
-        et_name = (EditText) findViewById(R.id.etName);
-        et_tel = (EditText) findViewById(R.id.etTelephone);
-        et_add = (EditText) findViewById(R.id.etAddress);
+        et_Email = findViewById(R.id.etEmail);
+        et_pass = findViewById(R.id.etPassword);
+        et_name = findViewById(R.id.etName);
+        et_tel = findViewById(R.id.etTelephone);
+        et_add = findViewById(R.id.etAddress);
 
-        b_checkEmail = (Button) findViewById(R.id.btn_CheckEmail);
-        b_checkPass = (Button) findViewById(R.id.btn_checkPass);
-        b_done = (Button) findViewById(R.id.btnDone);
-        b_back = (Button) findViewById(R.id.btnCancel);
+        tv_validEmail = findViewById(R.id.validEmail);
+        tv_validPass = findViewById(R.id.validPass);
 
-        // ischecked ???
-        rb_policyDec = (RadioButton) findViewById(R.id.radioPolicy_Dec);
-        rb_policyAcc = (RadioButton) findViewById(R.id.radioPolicy_Acc);
+        b_done = findViewById(R.id.btnDone);
+        b_back = findViewById(R.id.btnCancel);
+
+        rg_policy = findViewById(R.id.radioPolicy);
+
+        // 아이디 입력이 변경됨에 따라 2자 이상과 중복 체크
+        et_Email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!Pattern.matches("^().{2,20}$", et_Email.getText().toString())){
+                    tv_validEmail.setText("2자 이상 입력");
+                    bool_checkEmail = false;
+                } else {
+                    readFile(fileName);
+                }
+            }
+        });
+
+        // 비밀번호 입력이 변경됨에 따라 유효성 체크
+        et_pass.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // 비밀번호 유효성
+                if(!Pattern.matches("^(?=.*\\d)(?=.*[~`!@#$%\\^&*()-])(?=.*[a-zA-Z]).{8,20}$", et_pass.getText().toString()))
+                {
+                    tv_validPass.setText("부적합 비밀번호");
+                    bool_checkPass = false;
+                } else {
+                    tv_validPass.setText("유효한 비밀번호");
+                    bool_checkPass = true;
+                }
+            }
+        });
 
         // 회원가입 버튼
         b_done.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // member.txt에 띄어쓰기를 기준으로 회원아이디와 비밀번호 저장.
+                // member.txt에 띄어쓰기를 기준으로 회원아이디와 비밀번호, 이름, 전화번호, 주소 저장.
                 saveFile("member.txt", et_Email.getText().toString()+ " " + et_pass.getText().toString()
                         + " " + et_name.getText().toString()+ " " + et_tel.getText().toString()+ " " + et_add.getText().toString());
             }
@@ -58,14 +107,14 @@ public class SignonPage extends Activity {
         // 뒤로가기 버튼
         b_back.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), FirstPage.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                onBackPressed();
             }
         });
     }
 
-    public String readFile(String file) {
+    // 파일 읽으며 중복 체크 함수.
+    public void readFile(String file) {
         String text = "";
 
         try {
@@ -75,24 +124,47 @@ public class SignonPage extends Activity {
             fis.read(buffer);
             fis.close();
             text = new String(buffer);
+            // 읽은 텍스트 파일에서 map에 key로 id값을, value로 비밀번호를 넣는다.
+            splitMember = text.split("\n");
+            for(int i = 0; i < splitMember.length; i++) {
+                memberIDs[i] = splitMember[i].split(" ")[0];
+                if( memberIDs[i].equals(et_Email.getText().toString())){
+                    bool_checkEmail = false;
+                    tv_validEmail.setText("중복된 아이디");
+                    break;
+                }
+                else{
+                    bool_checkEmail = true;
+                    tv_validEmail.setText("가능한 아이디");
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
-            Toast.makeText(SignonPage.this, "Error reading file!", Toast.LENGTH_SHORT).show();
+            // 파일읽기는 성공하였으나 멤버가 아무도 없을 때를 위해 가입 가능하도록 설정.
+            bool_checkEmail = true;
+            tv_validEmail.setText("가능한 아이디");
         }
-
-        return text;
     }
 
+    // 회원가입하며 파일 저장 함수.
     public void saveFile(String file, String text) {
-        try {
-            text += "\n";
-            FileOutputStream fos = openFileOutput(file, Context.MODE_APPEND);
-            fos.write(text.getBytes());
-            fos.close();
-            Toast.makeText(SignonPage.this, "Saved!", Toast.LENGTH_SHORT).show();
-        } catch (Exception e) {
-            e.printStackTrace();
-            Toast.makeText(SignonPage.this, "Error saving file!", Toast.LENGTH_SHORT).show();
+        // 간략 약관 동의 여부 여기서 따져줌.
+        int id = rg_policy.getCheckedRadioButtonId();
+        RadioButton rb = findViewById(id);
+        if (bool_checkEmail == true && bool_checkPass == true && rb.getText().toString().equals("Accept")) {
+            try {
+                text += "\n";
+                FileOutputStream fos = openFileOutput(file, Context.MODE_APPEND);
+                fos.write(text.getBytes());
+                fos.close();
+                Toast.makeText(SignonPage.this, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show();
+                onBackPressed();
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(SignonPage.this, "회원가입에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(SignonPage.this, "회원가입에 실패하였습니다.", Toast.LENGTH_SHORT).show();
         }
     }
 }
